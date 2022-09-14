91aa57535133f1c1ec49c5d24dfad4b44787503c
aranramesh <svramesh692@gmail.com>
2022-09-14 10:41:26 +0530

    "bikin Website"

--- Business Website/HeroBiz/indesx.js
+++ Business Website/HeroBiz/indesx.js
@@ -1,26 +1,35 @@
-const addinput = document.querySelector('#addinput');
+// const addinput = document.querySelector('#addinput');
 
-const addbtn = dovument.querySelector('#addbtn');
+// const addbtn = dovument.querySelector('#addbtn');
 
-function addlists(){
-    if(addinput.value ==== ''){
-        alert('Enter The Name Pls')
-    }else {
-        const ul = divlist.querySelector('ul');
-        const li = document.createElement('li');
-        li.innerHTML='';
-        addinput.value ='';
-        ul.appendchild(li);
-    }
-}
+// function addlists(){
+//     if(addinput.value ==== ''){
+//         alert('Enter The Name Pls')
+//     }else {
+//         const ul = divlist.querySelector('ul');
+//         const li = document.createElement('li');
+//         li.innerHTML='';
+//         addinput.value ='';
+//         ul.appendchild(li);
+//     }
+// }
 
 
-addbtn.addEventListener('click',() =>{
-    addlists();
-});
+// addbtn.addEventListener('click',() =>{
+//     addlists();
+// });
 
-addinput.addEventListener('keyup',(evvent) =>{
-    if(event.which===13){
-        addlists();
-    }
-})
+// addinput.addEventListener('keyup',(evvent) =>{
+//     if(event.which===13){
+//         addlists();
+//     }
+// })
+
+let num1 = 10;
+
+let num2 = 5 ;
+
+const out = num1+num2;
+console.log(out)
+let out = 1+4;
+
