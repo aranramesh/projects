c7b2e272d541ed44b2085690cc11d65e96ead393
aranramesh <svramesh692@gmail.com>
2022-09-16 16:49:27 +0530

    bikin web page

--- Bikin/css/main.js
+++ Bikin/css/main.js
@@ -3,8 +3,78 @@
 // } )
 
 
-document.getElementById("myBtn").addEventListener("click", displayDate);
+// document.getElementById("myBtn").addEventListener("click", displayDate);
 
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-  }
+// function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+//   }
+
+
+// var p = prompt('enter a number');
+
+// var y = prompt('enter a number');
+
+// var v = prompt('enter a number');
+
+// var si = (p*y*v)/100;
+// console.log(si)
+
+  //  simple interest
+// var principle = document.getElementById("principle");
+// var roi = document.getElementById("roi");
+// var years = document.getElementById("years");
+// var outputspan = document.getElementById("outputspan");
+
+// //itvoyagers.in
+// function countSI()
+// {
+// 	p = parseFloat(principle.value);
+// 	n = parseFloat(years.value);
+// 	r = parseFloat(roi.value);
+// 	si = (p*n*r)/100;
+// 	outputspan.innerHTML = si;
+// }
+
+// //JavaScript program to swap two variables
+
+// //take input from the users
+// let a = prompt('Enter the first variable: ');
+// let b = prompt('Enter the second variable: ');
+
+// //create a temporary variable
+// let temp;
+
+// // //swap variables
+// // temp = a;
+// // a = b;
+// // b = temp;
+
+// // console.log(`The value of a after swapping: ${a}`);
+// // console.log(`The value of b after swapping: ${b}`);
+
+// var eng = 83;
+// var tam = 93;
+// var mat = 75;
+// var sci = 85;
+// var soc = 99;
+
+// let avg = eng +tam+mat+sci+soc;
+
+// let average = avg/500*100
+// console.log(average)
+
+// var x = prompt('number');
+
+// var y = prompt('number');
+
+// var out = Math.max(x,y)
+
+// console.log(out);
+
+// var num = prompt('number');
+
+// if(num > 10){
+//   console.log("greater than 10")
+// }else{
+//   console.log("low")
+// }
